f82cee3fbbdb4aca39dd3bcddda9bded4b017fcf
Chretien <Vous@exemple.com>
2025-01-24 15:37:10 +0100

    fintp

--- get-artifacts1.sh
+++ get-artifacts1.sh
@@ -1,5 +1,15 @@
+ID=$1 
+
 curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $(~/token.gh)" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/Exwyre/graphes/actions/artifacts
+
+
+
+curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $(~/token.gh)" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/Exwyre/graphes/actions/artifacts/$ID/zip --output artifact_$ID.zip
